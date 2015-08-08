855a3b0ce0d527a0e2169f43a8ecc3366ccda7ca
utpal <coolutpaljha@gmail.com>
2015-08-09 03:50:06 +0530

    First commit

---
+++ app/src/main/java/com/example/broadcasting/floatingchat.java
@@ -0,0 +1,72 @@
+package com.example.broadcasting;
+
+import android.app.Activity;
+import android.app.Service;
+//import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class floatingchat extends Activity 
+{
+
+	Button btn1,btn;
+	EditText ed1,ed2,ed3;
+	private Service servicing;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		setContentView(R.layout.floatinglayout);
+		
+				
+				btn=(Button)findViewById(R.id.bt1);
+				btn1=(Button)findViewById(R.id.bt2);
+				ed1=(EditText)findViewById(R.id.ed1);
+				ed2=(EditText)findViewById(R.id.ed2);
+				ed3=(EditText)findViewById(R.id.ed3);
+		
+	}
+
+	@Override
+	protected void onResume() {
+		
+		servicing = new Service()
+		{
+
+			@Override
+			public IBinder onBind(Intent intent) 
+			{
+				final String finalnumber = intent.getStringExtra("sender");
+			    final String finalstring = intent.getStringExtra("sms");
+			    ed1.setText(finalnumber);
+			    ed2.setText(finalstring);
+				return null;
+			}
+			
+			
+			
+			
+			
+		};
+		
+		
+		
+		super.onResume();
+	}
+	
+
+	@Override
+	protected void onPause()
+	{
+	super.onPause();
+	
+	}
+	
+	
+   
+	
+}
+
